073d7564c9a0989d0e49c08f6cb2f1e26b2cdb01
lukai <161871894+lu-kea@users.noreply.github.com>
2024-08-18 21:40:41 +0800

    Create myscript.js

---
+++ js/myscript.js
@@ -0,0 +1,38 @@
+//新增数据函数
+function add_data(){
+    var table=document.getElementById('table');
+    //console.log(table);
+    //获取插入位置
+    var length=table.rows.length;
+    //console.log(length);
+    var newRom=table.insertRow(length);
+    console.log(newRom);
+
+    var nameCell=newRom.insertCell(0);
+    var phoneCell=newRom.insertCell(1);
+    var actionCell=newRom.insertCell(2);
+    console.log(newRom);
+
+    nameCell.innerHTML =' ';
+    phoneCell.innerHTML =' ';
+    actionCell.innerHTML ='<button onclick="edit_data(this)">编辑</button><button onclick="delete_data(this)">删除</button>';
+}
+
+//删除数据函数
+function delete_data(button){
+    var row=button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+//编辑函数
+function edit_data(button){
+    var row=button.parentNode.parentNode;
+    var name=row.cells[0];
+    var phone=row.cells[1];
+
+    var inputName=prompt('请输入姓名');
+    var inputPhone=prompt('请输入联系方式');
+
+    name.innerHTML=inputName;
+    phone.innerHTML=inputPhone;
+}
